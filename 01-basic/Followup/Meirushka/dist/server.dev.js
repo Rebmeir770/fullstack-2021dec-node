01d7d57885e05b038fb20a4152193d34eb48a5d9
Meir Duvid Krykun <97189724+Rebmeir770@users.noreply.github.com>
2022-05-13 01:28:58 +0300

    start

--- 01-basic/Followup/Meirushka/dist/server.dev.js
+++ 01-basic/Followup/Meirushka/dist/server.dev.js
@@ -9,9 +9,22 @@
 var express = require('express');
 
 var app = express();
-var port = 5000;
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+var port = 4000;
+app.use(express["static"]('public')); // route
+
+app.get("/", function (req, res) {
+  res.send("Hello World!");
+});
+app.get("/bla", function (req, res) {
+  res.send("bla bla");
+});
+
+function multi(a, b) {
+  return a * b;
+}
+
+app.get('/multi', function (req, res) {
+  res.send("<h1>".concat(multi(8, 9), "</h1>"));
 });
 app.listen(port, function () {
   console.log("Server listening on port ".concat(port));
--- 01-basic/Followup/Meirushka/server.js
+++ 01-basic/Followup/Meirushka/server.js
@@ -9,13 +9,29 @@
 
 var express = require('express')
 var app = express()
-const port = 5000;
+const port = 4000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+app.use(express.static('public'));
+
+// route
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+
+app.get("/bla", (req, res) => {
+    res.send("bla bla");
+});
+
+function multi(a,b){
+   return a*b; 
+}
+
+app.get('/multi', (req, res) =>{
+    res.send(`<h1>${multi(8, 9)}</h1>`)
+});
+
 
 app.listen(port, () => {
-    console.log(`Server listening on port ${port}`)
-})
+    console.log(`Server listening on port ${port}`);
+});
 
