b6d4fff5580d5ba631733b8f93a8c604561a7c15
Meir Duvid Krykun <97189724+Rebmeir770@users.noreply.github.com>
2022-05-24 10:08:08 +0300

    api1

--- 01-basic/Exercise1/Meir/dist/server.dev.js
+++ 01-basic/Exercise1/Meir/dist/server.dev.js
@@ -3,7 +3,7 @@
 var express = require('express');
 
 var app = express();
-var port = process.env.PORT || 4000;
+var port = process.env.PORT || 4500;
 app.use(express["static"]('public'));
 app.listen(port, function () {
   console.log("Server listening on port ".concat(port));
